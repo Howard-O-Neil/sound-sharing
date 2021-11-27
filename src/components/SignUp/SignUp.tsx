c3935b26603933ab5fc862dea3b5fc710ceb3ab9
mingkhoi <mingkhoitran1234@gmail.com>
2021-11-27 15:40:37 +0700

    signup component

--- src/components/SignUp/SignUp.tsx
+++ src/components/SignUp/SignUp.tsx
@@ -31,7 +31,7 @@ const SignUp = () => {
         <p>----------</p>
       </div>
 
-      <div className={styles["login-div"]}>
+      <div className={styles["signup-div"]}>
         <form onSubmit={handleSubmit} className={styles["form-wrap"]}>
           <label>Email address</label> <br/>
           
@@ -40,25 +40,18 @@ const SignUp = () => {
           <label>Password</label> <br/>
           
           <input name="pass" type="text" /> <br/>
-          
-          <input name="remember" type="checkbox" />
-          <label>Remember me</label>
 
-          <input className={styles["submit-btn"]} type="submit" value="Login" /> <br/>
+          <label>Confirm your password</label> <br/>
+          
+          <input name="confirm-pass" type="text" /> <br/>
+
+          <label>What should we call you?</label> <br/>
+          
+          <input name="profile-name" type="text" /> <br/>
+
+          <input className={styles["submit-btn"]} type="submit" value="Sign up" /> <br/>
         </form>
       </div>
-
-      <br />
-      <a href="#">Forgot your password?</a>
-
-      <br /> <br /> <br />
-      <hr className={styles["short-ruler"]}></hr>
-
-      <div className={styles.heading}>You don't have account?</div>
-      <button>
-          <i className="fas fa-2x fa-user-plus"></i>
-          Sign up for SoundSharing
-        </button>
     </div>
   );
 };
